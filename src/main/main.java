6d4548a22f29868994f2bddf185cae8c2803ca98
NeoD1m <Duffman7915492@gmail.com>
2020-09-15 23:22:33 +0300

    parse name,number of pages and url of pictures

---
+++ src/main/main.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        com.company.WebParser.parse(177013);
+    }
+}
